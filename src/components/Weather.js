b7a2d9439f3d221928ac4d7d2f05f42f52404b60
brianminges <btminges@gmail.com>
2022-11-05 14:46:44 -0500

    early CSS

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,7 +6,6 @@ import "./weather.css"
 export const Weather = () => {
     const [weather, setWeather] = useState({})
     const API = process.env.REACT_APP_WEATHER_KEY
-    // const API = "a5aa37f380abaf6f1c20a1d27119fd00"
     const { coords, isGeolocationAvailable, isGeolocationEnabled } =
     
     useGeolocated({
@@ -20,7 +19,16 @@ export const Weather = () => {
             if (coords) {
               getWeather(coords, API).then(setWeather)
             }
+            console.log('weather', weather)
           }, [coords])
+
+    const [description, setDescription] = useState("")
+
+    // useEffect(() => {
+    //     weather.weather.map(w => (
+    //         setDescription(w.main)
+    //     ))  
+    // }, [weather])
 
 return !isGeolocationAvailable ? (
     <div>Your browser does not support Geolocation</div>
@@ -29,14 +37,20 @@ return !isGeolocationAvailable ? (
 ) : coords ? (
     
     <React.Fragment>
-    <div> 
+    <div className="content"> 
     <h1>Today in {weather?.name}</h1>
     <p>Current: {Math.round(weather?.main?.temp)}&#176;F</p>
     {/* <p>{weather.weather[0].main}</p> */}
+ 
+    {/* <ul>{weather.weather.map(w => (
+        <ul>{w.main}</ul>
+    ))} </ul> */}
+
     <p>High/low: {Math.round(weather?.main?.temp_max)}&#176;F / {Math.round(weather?.main?.temp_min)}&#176;F</p>
     <p>Humidity: {weather?.main?.humidity}%</p>
     <p>Feels like: {Math.round(weather?.main?.feels_like)}&#176;F</p>
     <p>Wind: {Math.round(weather?.wind?.speed)} mph</p>
+
     </div>
     </React.Fragment>
     ) : (
